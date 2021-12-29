67ad5852b92b6c9fa4ba9cdcbdc4066679ecc8be
Sebasitna GC <sebastian@pop-os.localdomain>
2021-12-29 01:54:24 -0600

    Added infinite scroll effect

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Image from "next/image";
-import styles from "../styles/Home.module.scss";
+import Link from "next/link";
 import IconSearch from "../components/icons/IconSearch";
 import MainLayout from "../components/layouts/MainLayout";
+import Loader from "../components/Loader";
+import styles from "../styles/Home.module.scss";
 import logoRickandMorty from "../public/logo.png";
-import Link from "next/link";
 
 export async function getStaticProps() {
   const res = await fetch("https://rickandmortyapi.com/api/character");
@@ -21,17 +22,33 @@ export async function getStaticProps() {
 
 export default function Home({ nextPage, data }) {
   const [characters, setCharacters] = useState(data);
-  const [filtedCharacters, setFiltedCharacters] = useState(characters);
+  const [filtedCharacters, setFiltedCharacters] = useState(data);
   const [nextUrl, setNextUrl] = useState(nextPage);
   const [search, setSearch] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const loadMoreElement = useRef(null);
 
   const getMoreCharacters = async () => {
+    setIsLoading(true);
     const res = await fetch(nextUrl);
     const newData = await res.json();
 
     setCharacters([...characters, ...newData.results]);
     setNextUrl(newData.info.next);
+    setIsLoading(false);
   };
+
+  useEffect(() => {
+    const options = { rootMargin: "300px" };
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) getMoreCharacters();
+    }, options);
+
+    observer.observe(loadMoreElement.current);
+
+    return () => observer.disconnect();
+  });
 
   useEffect(() => {
     setFiltedCharacters(
@@ -85,13 +102,15 @@ export default function Home({ nextPage, data }) {
             </a>
           </Link>
         ))}
-        <button
-          className={styles.button_loading_more}
-          onClick={getMoreCharacters}
-        >
-          Load more
-        </button>
       </section>
+
+      <div ref={loadMoreElement}></div>
+
+      {isLoading && (
+        <div className={styles.loader_wrapper}>
+          <Loader />
+        </div>
+      )}
     </MainLayout>
   );
 }
